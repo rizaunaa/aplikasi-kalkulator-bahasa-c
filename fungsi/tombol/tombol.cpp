d15d25e62e3e442a1931775f02cf861800be9889
Riza <rizauna94@gmail.com>
2026-02-22 06:28:27 +0700

    tambah tombol operasional mtk

---
+++ fungsi/tombol/tombol.cpp
@@ -0,0 +1,94 @@
+#include "tombol.h"
+
+namespace {
+
+struct TombolDef {
+    int id;
+    const char* teks;
+    int row;
+    int col;
+    int rowSpan;
+    int colSpan;
+    HWND hwnd;
+};
+
+constexpr int kTotalCols = 4;
+constexpr int kTotalRows = 5;
+constexpr int kMargin = 40;
+constexpr int kGap = 10;
+constexpr int kDisplayTop = 40;
+constexpr int kDisplayHeight = 90;
+
+TombolDef gTombol[] = {
+    { ID_BTN_C, "C", 0, 0, 1, 1, nullptr },
+    { ID_BTN_BACKSPACE, "<-", 0, 1, 1, 1, nullptr },
+    { ID_BTN_BAGI, "/", 0, 2, 1, 1, nullptr },
+    { ID_BTN_KALI, "*", 0, 3, 1, 1, nullptr },
+    { ID_BTN_7, "7", 1, 0, 1, 1, nullptr },
+    { ID_BTN_8, "8", 1, 1, 1, 1, nullptr },
+    { ID_BTN_9, "9", 1, 2, 1, 1, nullptr },
+    { ID_BTN_KURANG, "-", 1, 3, 1, 1, nullptr },
+    { ID_BTN_4, "4", 2, 0, 1, 1, nullptr },
+    { ID_BTN_5, "5", 2, 1, 1, 1, nullptr },
+    { ID_BTN_6, "6", 2, 2, 1, 1, nullptr },
+    { ID_BTN_TAMBAH, "+", 2, 3, 1, 1, nullptr },
+    { ID_BTN_1, "1", 3, 0, 1, 1, nullptr },
+    { ID_BTN_2, "2", 3, 1, 1, 1, nullptr },
+    { ID_BTN_3, "3", 3, 2, 1, 1, nullptr },
+    { ID_BTN_SAMA_DENGAN, "=", 3, 3, 2, 1, nullptr },
+    { ID_BTN_0, "0", 4, 0, 1, 2, nullptr },
+    { ID_BTN_KOMA, ",", 4, 2, 1, 1, nullptr }
+};
+
+}  // namespace
+
+// Membuat semua tombol kalkulator sebagai child window.
+void BuatTombolKalkulator(HWND parent) {
+    for (auto& tombol : gTombol) {
+        tombol.hwnd = CreateWindowExA(
+            0,
+            "BUTTON",
+            tombol.teks,
+            WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            0,
+            0,
+            0,
+            0,
+            parent,
+            reinterpret_cast<HMENU>(static_cast<INT_PTR>(tombol.id)),
+            GetModuleHandle(nullptr),
+            nullptr
+        );
+    }
+}
+
+// Menghitung dan menerapkan posisi tombol dalam grid.
+void TataLetakTombol(HWND parent, const RECT& clientRect) {
+    const int clientWidth = clientRect.right - clientRect.left;
+    const int clientHeight = clientRect.bottom - clientRect.top;
+    const int gridTop = kDisplayTop + kDisplayHeight + 30;
+    const int usableWidth = clientWidth - (kMargin * 2);
+    const int usableHeight = clientHeight - gridTop - kMargin;
+
+    if (usableWidth <= 0 || usableHeight <= 0) {
+        return;
+    }
+
+    const int cellWidth = (usableWidth - (kGap * (kTotalCols - 1))) / kTotalCols;
+    const int cellHeight = (usableHeight - (kGap * (kTotalRows - 1))) / kTotalRows;
+
+    for (auto& tombol : gTombol) {
+        if (!tombol.hwnd) {
+            continue;
+        }
+
+        const int x = kMargin + tombol.col * (cellWidth + kGap);
+        const int y = gridTop + tombol.row * (cellHeight + kGap);
+        const int w = tombol.colSpan * cellWidth + (tombol.colSpan - 1) * kGap;
+        const int h = tombol.rowSpan * cellHeight + (tombol.rowSpan - 1) * kGap;
+
+        MoveWindow(tombol.hwnd, x, y, w, h, TRUE);
+    }
+
+    InvalidateRect(parent, nullptr, TRUE);
+}
---
+++ fungsi/tombol/tombol.h
@@ -0,0 +1,34 @@
+#ifndef FUNGSI_TOMBOL_H
+#define FUNGSI_TOMBOL_H
+
+#include <windows.h>
+
+// ID tombol kalkulator.
+enum TombolId {
+    ID_BTN_C = 1001,
+    ID_BTN_BACKSPACE,
+    ID_BTN_BAGI,
+    ID_BTN_KALI,
+    ID_BTN_7,
+    ID_BTN_8,
+    ID_BTN_9,
+    ID_BTN_KURANG,
+    ID_BTN_4,
+    ID_BTN_5,
+    ID_BTN_6,
+    ID_BTN_TAMBAH,
+    ID_BTN_1,
+    ID_BTN_2,
+    ID_BTN_3,
+    ID_BTN_SAMA_DENGAN,
+    ID_BTN_0,
+    ID_BTN_KOMA
+};
+
+// Membuat semua tombol kalkulator.
+void BuatTombolKalkulator(HWND parent);
+
+// Mengatur posisi tombol saat ukuran window berubah.
+void TataLetakTombol(HWND parent, const RECT& clientRect);
+
+#endif
